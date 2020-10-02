6a4403b4d143dbe4f448f3ceb7b12bfcc1a3ee81
Josue Martinez <josue.martinez65@yahoo.com>
2020-10-02 15:47:39 -0700

    finished

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const transitioning = document.querySelector(".transitioning").innerHTML
+function modify(a, b, c) {
+  document.getElementById(a).style.display = "block"
+  document.getElementById(b).style.display = "none"
+  document.getElementById(c).style.display = "none"
+}
 
-document.addEventListener("mouseenter", getIt)
-
-let getIt = function trans() {}
